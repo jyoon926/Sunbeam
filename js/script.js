aa615f7052cb0a584c2f29f88bb577cac0747494
Jacob Yoon <jacobyoon926@gmail.com>
2020-07-26 19:53:11 -0400

    Fix buttons

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function strayed() {
+    var elmnt = document.getElementById("strayed");
+    elmnt.scrollIntoView();
+}
+function about() {
+    var elmnt = document.getElementById("about");
+    elmnt.scrollIntoView();
+}
